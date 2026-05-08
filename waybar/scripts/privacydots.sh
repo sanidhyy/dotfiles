61a1f362f0fe0abd5caac6e35f2cf97626ecd6ed
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2026-05-08 21:23:58 +0530

    refactor: simplify privacydots scripts

--- waybar/scripts/privacydots.sh
+++ waybar/scripts/privacydots.sh
@@ -42,8 +42,8 @@ orange="#FF9F0A"  # cam
 
 # build text string
 text=""
-[[ $mic -eq 1 ]] && text+="<span foreground=\"$green\">󰍬</span> "
-[[ $cam -eq 1 ]] && text+="<span foreground=\"$orange\"></span> "
+[[ $mic -eq 1 ]] && text+="<span foreground=\"$green\" size=\"larger\">󰍬</span> "
+[[ $cam -eq 1 ]] && text+="<span foreground=\"$orange\" size=\"larger\"></span> "
 text="${text% }"
 tooltip="Mic: $([[ $mic -eq 1 ]] && echo on || echo off) | Cam: $([[ $cam -eq 1 ]] && echo on || echo off)"
 
